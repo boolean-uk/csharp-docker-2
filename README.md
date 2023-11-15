85cb6a92bf035f5af0e897673cdbf253e616b4e6
Nigel <nigel.sibbert@gmail.com>
2023-11-15 14:29:21 +0000

    added exercise

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# csharp-docker-2
+# C# Docker Exercise 1
+
+## Learning Objectives  
+
+- Create a WebApi project with endpoints  
+- Deploy this project to a Docker Container  
+
+## Instructions  
+
+1. Fork this repository  
+2. Clone the fork to your machine  
+3. Open in Visual Studio  
+
+## Core 
+
+-Create endpoints in the project to GET/POST/PUT/DELETE a Product  
+
---
+++ exercise.wwwapi/Data/Seeder.cs
@@ -0,0 +1,21 @@
+﻿using exercise.wwwapi.Models;
+
+namespace exercise.wwwapi.Data
+{
+    public static class Seeder
+    {
+        public static List<Product> GetListOfProducts()
+        {
+            var products = new List<Product>()
+            {
+                new Product() {Name="Fender Stratocaster", Description="Electric Guitar", Price=1200.00m }
+                
+
+            };
+
+          
+
+            return products;
+        }
+    }
+}
